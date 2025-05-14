181522d2f2bfd9d97fdef8e34d544dce999f9636
Shoufeng Yang <shoufeng@bastion>
2025-05-14 19:54:58 +0000

    Delete top portion ofthe file

--- Find-VC.ps1
+++ Find-VC.ps1
@@ -1,36 +1,3 @@
-#requires -version 3.0
-
-<#
-.SYNOPSIS
-	Search VC's VM throw direct connection to group of ESXi Hosts.
-.DESCRIPTION
-	This script generate list of ESXi Hosts with common suffix in name,
-	e.g. (esxprod1,esxprod2, ...) or (esxdev01,esxdev02, ...) etc. and
-	search VCenter's VM throw direct connection to this group of ESXi Hosts.
-.PARAMETER VC
-	VC's VM Name.
-.PARAMETER HostSuffix
-	ESXi Hosts' common suffix.
-.PARAMETER PostfixStart
-	ESXi Hosts' postfix number start.
-.PARAMETER PostfixEnd
-	ESXi Hosts' postfix number end.
-.PARAMETER AddZero
-	Add ESXi Hosts' postfix leading zero to one-digit postfix (from 01 to 09).
-.EXAMPLE
-	C:\PS> .\Find-VC.ps1 vc1 esxprod 1 20 -AddZero
-.EXAMPLE
-	C:\PS> .\Find-VC.ps1 -VC vc1 -HostSuffix esxdev -PostfixEnd 6
-.EXAMPLE
-	C:\PS> .\Find-VC.ps1 vc1 esxprod |fl
-.NOTES
-	Author: Roman Gelman.
-.OUTPUTS
-	PSCustomObject with two Properties: VC,VMHost or $null.
-.LINK
-	https://ps1code.com
-#>
-
 Param (
 
 	[Parameter(Mandatory=$true,Position=1,HelpMessage="vCenter's VM Name")]
